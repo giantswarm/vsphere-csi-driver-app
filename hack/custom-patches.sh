842ad696f6b008866b1f2662bde8bab0432d04ae
Xavier <xavier.avrillier@gmail.com>
2025-12-19 12:18:34 +0100

    make affinity and toleration configurable, 3.3.1 update
    * more interfaces and 3.3.1 update
    
    * lint

--- hack/custom-patches.sh
+++ hack/custom-patches.sh
@@ -9,7 +9,12 @@ YQ="./bin/yq"
 csi_controller_manifest="helm/vsphere-csi-driver/templates/apps_v1_deployment_vsphere-csi-controller.yaml"
 csi_nodeplugin_manifest="helm/vsphere-csi-driver/templates/apps_v1_daemonset_vsphere-csi-node.yaml"
 
+# Remove existing affinity and tolerations added upstream since we want to control them via Helm values.
+${YQ} -i 'del(.spec.template.spec.affinity)' ${csi_controller_manifest}
+${YQ} -i 'del(.spec.template.spec.tolerations)' ${csi_controller_manifest}
+
 # Add SecurityContext to the containers.
+# Add interface to configure affinity and tolerations for the controller deployment.
 ${YQ} eval '.spec.template.spec.securityContext.remove-this-key="'"
 {{- with .Values.podSecurityContext }}
   {{- . | toYaml | nindent 8 }}
@@ -18,7 +23,16 @@ ${YQ} eval '.spec.template.spec.securityContext.remove-this-key="'"
 {{- with .Values.containerSecurityContext }}
   {{- . | toYaml | nindent 12 }}
 {{- end }}
-"'"' ${csi_controller_manifest} > ${csi_controller_manifest}.tmp
+"'" | .spec.template.spec.affinity.remove-this-key="'"
+{{- with .Values.controller.affinity }}
+  {{- . | toYaml | nindent 8 }}
+{{- end }}
+"'" | .spec.template.spec.tolerations.remove-this-key="'"
+{{- with .Values.controller.tolerations }}
+  {{- . | toYaml | nindent 8 }}
+{{- end }}
+"'"' \
+${csi_controller_manifest} > ${csi_controller_manifest}.tmp
 
 # Remove existing runAsNonRoot keys added upstream since we set it in the chart's values.
 # https://github.com/giantswarm/cloud-provider-vsphere-app/blob/6556f98de46ff45b3a8ce9080752ca1050bbee0b/helm/cloud-provider-vsphere/charts/vsphere-csi-driver/values.yaml#L59
